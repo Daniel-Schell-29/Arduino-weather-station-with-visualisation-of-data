4a5997fa1a34b5b94609c86139415d7d0a20aee5
Daniel-Schell-29 <86618549+Daniel-Schell-29@users.noreply.github.com>
2025-09-18 21:05:01 +0200

    added graphs main.py

--- python/main.py
+++ python/main.py
@@ -1,15 +1,48 @@
 import serial
 import time
+import matplotlib.pyplot as plt
 
-# Openinig serial connection 
+
+# Opening serial connection 
 ser =serial.Serial('COM3', 9600, timeout=1)
 time.sleep(2)  # wait for the serial connection to initialize
 
+val_history=[list() for _ in range(3)]#ldr,temp,humidity
+labels= ['LDR','Temp','Humidity']
+
+plt.ion()
+fig, axs = plt.subplots(3, 1, figsize=(8, 8)) #3 rows, 1 column
+
+errors=0 #count errors in a row
 while True:
-    line = ser.readline().decode('utf-8').rstrip()
+    line = ser.readline().decode('utf-8').rstrip()  #read serial input
     if line:
         try:
-            ldr, temp, hum =map(float, line.split(','))
-            print(f"LDR: {ldr}, Temperature: {temp}, Humidity: {hum}")
-        except ValueError:
+            
+            sensor =list( map(float, line.split(',')))  #put serial input in list
+            for data_list, val in zip(val_history, sensor):
+                data_list.append(val)
+            errors=0 #reset error count
+            print(val_history)
+            
+
+        except ValueError:  #stop programm if data is malformed
             print("Received malformed data:", line)
+            errors+=1
+            if errors>5: #after more than 5 errors in a row stop programm
+                print("Too many errors, stopping.")
+                ser.close()
+                break
+            continue
+
+  
+        for i,( data_list, name) in enumerate(zip(val_history , labels)):  #add Data to each plot
+            axs[i].clear() 
+            axs[i].plot(data_list[-100:],label=name)
+            axs[i].legend()
+            plt.pause(0.1)
+            
+            
+        if plt.get_fignums()== []:   #stop porgramm when plot window closed
+            ser.close()
+            break
